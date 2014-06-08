18763db64a84289536e13e3e82799d6461d54367
金子修一郎 <kanekoshuuichirou@kanekosrounombp.lan>
2014-06-09 02:29:12 +0900

    homework1-3 初回

--- HomeWork1-3/ViewController.m
+++ HomeWork1-3/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "UIViewController+NiceAnimation.h"
+
 
 @interface ViewController ()
 
@@ -18,6 +20,21 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+
+    static int index;
+    index++;
+    if(index>=5) index=0;
+    
+    NSString *filename = [NSString stringWithFormat:@"%d.jpg", index];
+    UIImage *img = [UIImage imageNamed:filename];
+    
+    self.navigationItem.rightBarButtonItem =
+            [[UIBarButtonItem alloc] initWithTitle:@"Close"
+            style:UIBarButtonItemStyleBordered target:self action:@selector(clickClose:)];
+
+
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:img];
+    [self.view insertSubview:imageView atIndex:0];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +42,21 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+- (IBAction)push:(id)sender
+{
+    //NSLog(@"aaaa");
+    ViewController *postViewController = [[ViewController alloc] init];
+    UINavigationController* navigationController = [[UINavigationController alloc] initWithRootViewController:postViewController];
+
+    [self presentViewController:navigationController animated:YES completion:nil];
+}
+
+- (void)clickClose:(id)sender
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
 
 @end
